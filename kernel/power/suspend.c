a7c5e61ea35b161be039f8ea7c268970977ab94a
Sultan Alsawaf <sultanxda@gmail.com>
2018-05-17 00:40:23 -0700

    qcom-cpufreq: Boost all online CPUs when exiting suspend
    In order to improve resume performance when the CPU is waking up from
    suspend (i.e., deep sleep), boost all online CPUs to their respective
    maximum frequencies for 20 ms. The power cost of boosting everything to
    the max for 20 ms is negligible, so this doesn't significantly increase
    power consumption.
    
    The boost is kicked off right before all the userspace processes are
    thawed, as that's when the boost is needed most.
    
    Signed-off-by: Sultan Alsawaf <sultanxda@gmail.com>
    Signed-off-by: Dusan Uveric <dusan.uveric9@gmail.com>

--- kernel/power/suspend.c
+++ kernel/power/suspend.c
@@ -32,6 +32,7 @@
 #include <linux/compiler.h>
 #include <linux/moduleparam.h>
 #include <linux/wakeup_reason.h>
+#include <linux/cpufreq.h>
 
 #include <linux/gpio.h>
 #include "power.h"
@@ -474,6 +475,7 @@ int suspend_devices_and_enter(suspend_state_t state)
  */
 static void suspend_finish(void)
 {
+	msm_do_pm_boost(true);
 	suspend_thaw_processes();
 	pm_notifier_call_chain(PM_POST_SUSPEND);
 	pm_restore_console();
