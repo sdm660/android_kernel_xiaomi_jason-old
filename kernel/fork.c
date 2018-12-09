9df5b2243e3348f851001173a2870f76c041c47d
kdrag0n <dragon@khronodragon.com>
2018-12-08 17:47:01 -0800

    kernel: don't boost CPUs on app launch when suspended
    Wastes battery. Why would the user care about how fast services start
    when they're not using the device?
    
    Signed-off-by: kdrag0n <dragon@khronodragon.com>
    Signed-off-by: Dusan Uveric <dusan.uveric9@gmail.com>

--- kernel/fork.c
+++ kernel/fork.c
@@ -81,6 +81,7 @@
 #include <linux/cpufreq_times.h>
 #include <linux/cpu_input_boost.h>
 #include <linux/devfreq_boost.h>
+#include <linux/display_state.h>
 
 #include <asm/pgtable.h>
 #include <asm/pgalloc.h>
@@ -1832,7 +1833,7 @@ long _do_fork(unsigned long clone_flags,
 	long nr;
 
 	/* Boost CPU to the max for 500 ms when userspace launches an app */
-	if (is_zygote_pid(current->pid)) {
+	if (is_zygote_pid(current->pid) && is_display_on()) {
 		cpu_input_boost_kick_max(32);
 		devfreq_boost_kick_max(DEVFREQ_MSM_CPUBW, 32);
 	}
