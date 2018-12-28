95ce95851c074bd110e671217e23745bc61df0c2
Minchan Kim <minchan@google.com>
2018-12-28 19:02:42 +0900

    sched: fix rcu_read_lock lockdep splat
    With lockdep, I encountered following warning.
    
    - find_lowest_rq calls idle_get_state_idx without rcu_read_lock
    
    rcu_read_lock is very cheap and find_lowest_rq should be fast so
    don't bother to hold/release the lock inside of find_lowest_rq,
    which is rather complicated. Instead, let's hold the lock outside
    of find_lowest_rq.
    
    It makes divergent from upstream but scheduler was already a lot
    devergent.
    
    [   35.234350] ------------[ cut here ]------------
    [   35.234379] WARNING: CPU: 0 PID: 687 at /usr/local/google/home/minchan/nvme/kernel/crosshatch-kernel/private/msm-google/kernel/sched/sched.h:1625 find_lowest_rq+0x750/0x75c
    [   35.234386] Modules linked in: sec_touch heatmap videobuf2_vmalloc videobuf2_memops snd_soc_sdm845 snd_soc_cs35l36 snd_soc_wcd_spi snd_soc_wcd934x snd_soc_wcd9xxx wcd_dsp_glink wcd_core pinctrl_wcd wlan(O)
    [   35.234447] CPU: 0 PID: 687 Comm: appEventThread Tainted: G        W  O    4.9.140-g18984bc09df9_audio-g3dce958 #11
    [   35.234454] Hardware name: Google Inc. MSM sdm845 C1 Proto v1.0 (DT)
    [   35.234461] task: fffffff8e71fd100 task.stack: fffffff8e529c000
    [   35.234468] PC is at find_lowest_rq+0x750/0x75c
    [   35.234477] LR is at find_lowest_rq+0x69c/0x75c
    [   35.234483] pc : [<ffffff90e410a9ec>] lr : [<ffffff90e410a938>] pstate: 604001c5
    ..
    ..
    [   35.235182] [<ffffff90e410a9ec>] find_lowest_rq+0x750/0x75c
    [   35.235191] [<ffffff90e410aaa8>] push_rt_task+0xb0/0x32c
    [   35.235199] [<ffffff90e410ad3c>] push_rt_tasks+0x18/0x28
    [   35.235210] [<ffffff90e40e2920>] __balance_callback+0x3c/0x60
    [   35.235221] [<ffffff90e516b88c>] __schedule+0x4dc/0x9ec
    [   35.235228] [<ffffff90e516c0a8>] schedule+0x34/0x94
    [   35.235238] [<ffffff90e4165714>] futex_wait_queue_me+0xd0/0x178
    [   35.235246] [<ffffff90e41664d4>] futex_wait+0xe4/0x204
    [   35.235254] [<ffffff90e416860c>] do_futex+0x16c/0x84c
    [   35.235263] [<ffffff90e4168d7c>] SyS_futex+0x90/0x1bc
    [   35.235272] [<ffffff90e4083a00>] el0_svc_naked+0x34/0x38
    
    Bug: 122097578
    Change-Id: I12e4c4987e51c5edc5317b93144453e47680e43c
    Signed-off-by: Minchan Kim <minchan@google.com>
    Signed-off-by: celtare21 <celtare21@gmail.com>
    
    Signed-off-by: Dusan Uveric <dusan.uveric9@gmail.com>

--- kernel/sched/rt.c
+++ kernel/sched/rt.c
@@ -2014,7 +2014,6 @@ static int find_lowest_rq(struct task_struct *task, int sync)
 		struct task_struct* curr;
 		if (!cpumask_test_cpu(this_cpu, lowest_mask))
 			this_cpu = -1; /* Skip this_cpu opt if not among lowest */
-		rcu_read_lock();
 		for_each_domain(cpu, sd) {
 			if (sd->flags & SD_WAKE_AFFINE) {
 				int best_cpu;
@@ -2030,7 +2029,6 @@ static int find_lowest_rq(struct task_struct *task, int sync)
 					 * priority*/
 					if (!curr || (schedtune_task_boost(curr) == 0
 					    && schedtune_prefer_idle(curr) == 0)) {
-						rcu_read_unlock();
 						return this_cpu;
 					}
 				}
@@ -2044,7 +2042,6 @@ static int find_lowest_rq(struct task_struct *task, int sync)
 					 * priority*/
 					if(!curr || (schedtune_task_boost(curr) == 0
 						     && schedtune_prefer_idle(curr) == 0)) {
-						rcu_read_unlock();
 						return best_cpu;
 					}
 				}
@@ -2074,7 +2071,9 @@ static struct rq *find_lock_lowest_rq(struct task_struct *task, struct rq *rq)
 	int cpu;
 
 	for (tries = 0; tries < RT_MAX_TRIES; tries++) {
-		cpu = find_lowest_rq(task, 0);
+		rcu_read_lock();
+                cpu = find_lowest_rq(task, 0);
+		rcu_read_unlock();
 
 		if ((cpu == -1) || (cpu == rq->cpu))
 			break;
