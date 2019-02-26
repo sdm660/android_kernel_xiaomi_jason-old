65dce87e1650755b1277912de1467696d6534061
Tyler Nijmeh <tylernij@gmail.com>
2019-02-26 12:26:27 -0800

    cpu_input_boost: Determine state in boost functions
    Instead of passing the state variable, we can find it in the boost
    function itself. This cleans up the function and also improves memory
    management.
    
    Signed-off-by: Tyler Nijmeh <tylernij@gmail.com>
    Signed-off-by: Dusan Uveric <dusan.uveric9@gmail.com>

--- drivers/cpufreq/cpu_input_boost.c
+++ drivers/cpufreq/cpu_input_boost.c
@@ -27,10 +27,6 @@ module_param(suspend_stune_boost, int, 0644);
 #define GENERAL_STUNE_BOOST	BIT(1)
 #define SUSPEND_STUNE_BOOST	BIT(2)
 
-#define ST_TOP_APP		BIT(0)
-#define ST_FOREGROUND		BIT(1)
-#define ST_BACKGROUND		BIT(2)
-
 struct boost_drv {
 	struct workqueue_struct *wq;
 	struct work_struct input_boost;
@@ -74,48 +70,35 @@ static void clear_boost_bit(struct boost_drv *b, u32 state)
 	atomic_andnot(state, &b->state);
 }
 
-static void set_stune_boost(struct boost_drv *b, u32 state, u32 bit, u32 st, int level,
-			    int *slot)
+static void set_stune_boost(struct boost_drv *b, u32 bit, int level, int *slot)
 {
+	u32 state = get_boost_state(b);
+
 	if (level && !(state & bit)) {
-		if ((st & ST_TOP_APP) && !do_stune_boost("top-app", level, slot))
-			set_boost_bit(b, bit);
-
-		if ((st & ST_FOREGROUND) && !do_stune_boost("foreground", level, slot))
-			set_boost_bit(b, bit);
-
-		if ((st & ST_BACKGROUND) && !do_stune_boost("background", level, slot))
+		if (!do_stune_boost("top-app", level, slot))
 			set_boost_bit(b, bit);
 	}
 }
 
-static void clear_stune_boost(struct boost_drv *b, u32 state, u32 bit, u32 st, int slot)
+static void clear_stune_boost(struct boost_drv *b, u32 bit, int slot)
 {
+	u32 state = get_boost_state(b);
+
 	if (state & bit) {
-		if (st & ST_TOP_APP)
-			reset_stune_boost("top-app", slot);
-
-		if (st & ST_TOP_APP)
-			reset_stune_boost("foreground", slot);
-
-		if (st & ST_TOP_APP)
-			reset_stune_boost("background", slot);
-
+		reset_stune_boost("top-app", slot);
 		clear_boost_bit(b, bit);
 	}
 }
 
 static void unboost_all_cpus(struct boost_drv *b)
 {
-	u32 state = get_boost_state(b);
-
 	if (!cancel_delayed_work_sync(&b->input_unboost) &&
 		!cancel_delayed_work_sync(&b->general_unboost))
 		return;
 
-	clear_stune_boost(b, state, INPUT_STUNE_BOOST, ST_TOP_APP | ST_FOREGROUND | ST_BACKGROUND, b->input_stune_slot);
-	clear_stune_boost(b, state, GENERAL_STUNE_BOOST, ST_TOP_APP | ST_FOREGROUND | ST_BACKGROUND, b->general_stune_slot);
-	clear_stune_boost(b, state, SUSPEND_STUNE_BOOST, ST_TOP_APP | ST_FOREGROUND | ST_BACKGROUND, b->suspend_stune_slot);
+	clear_stune_boost(b, INPUT_STUNE_BOOST, b->input_stune_slot);
+	clear_stune_boost(b, GENERAL_STUNE_BOOST, b->general_stune_slot);
+	clear_stune_boost(b, SUSPEND_STUNE_BOOST, b->suspend_stune_slot);
 }
 
 void cpu_input_boost_kick(void)
@@ -168,10 +151,9 @@ void cpu_general_boost_kick(unsigned int duration_ms)
 static void input_boost_worker(struct work_struct *work)
 {
 	struct boost_drv *b = container_of(work, typeof(*b), input_boost);
-	u32 state = get_boost_state(b);
 
 	if (!cancel_delayed_work_sync(&b->input_unboost)) {
-		set_stune_boost(b, state, INPUT_STUNE_BOOST, ST_TOP_APP, input_stune_boost,
+		set_stune_boost(b, INPUT_STUNE_BOOST, input_stune_boost,
 			&b->input_stune_slot);
 	}
 
@@ -182,10 +164,9 @@ static void input_boost_worker(struct work_struct *work)
 static void general_boost_worker(struct work_struct *work)
 {
 	struct boost_drv *b = container_of(work, typeof(*b), general_boost);
-	u32 state = get_boost_state(b);
 
 	if (!cancel_delayed_work_sync(&b->general_unboost)) {
-		set_stune_boost(b, state, GENERAL_STUNE_BOOST, ST_TOP_APP, general_stune_boost,
+		set_stune_boost(b, GENERAL_STUNE_BOOST, general_stune_boost,
 			&b->general_stune_slot);
 	}
 
@@ -197,18 +178,16 @@ static void input_unboost_worker(struct work_struct *work)
 {
 	struct boost_drv *b =
 		container_of(to_delayed_work(work), typeof(*b), input_unboost);
-	u32 state = get_boost_state(b);
 
-	clear_stune_boost(b, state, INPUT_STUNE_BOOST, ST_TOP_APP, b->input_stune_slot);
+	clear_stune_boost(b, INPUT_STUNE_BOOST, b->input_stune_slot);
 }
 
 static void general_unboost_worker(struct work_struct *work)
 {
 	struct boost_drv *b =
 		container_of(to_delayed_work(work), typeof(*b), general_unboost);
-	u32 state = get_boost_state(b);
 
-	clear_stune_boost(b, state, GENERAL_STUNE_BOOST, ST_TOP_APP, b->general_stune_slot);
+	clear_stune_boost(b, GENERAL_STUNE_BOOST, b->general_stune_slot);
 }
 
 static int fb_notifier_cb(struct notifier_block *nb,
@@ -217,7 +196,6 @@ static int fb_notifier_cb(struct notifier_block *nb,
 	struct boost_drv *b = container_of(nb, typeof(*b), fb_notif);
 	struct fb_event *evdata = data;
 	int *blank = evdata->data;
-	u32 state = get_boost_state(b);
 
 	/* Parse framebuffer blank events as soon as they occur */
 	if (action != FB_EARLY_EVENT_BLANK)
@@ -225,10 +203,10 @@ static int fb_notifier_cb(struct notifier_block *nb,
 
 	/* Unboost when the screen turns off */
 	if (*blank == FB_BLANK_UNBLANK) {
-		clear_stune_boost(b, state, SUSPEND_STUNE_BOOST, ST_TOP_APP | ST_FOREGROUND | ST_BACKGROUND, b->suspend_stune_slot);
-	} else {
+		clear_stune_boost(b, SUSPEND_STUNE_BOOST, b->suspend_stune_slot);
+	} else {	
 		unboost_all_cpus(b);
-		set_stune_boost(b, state, SUSPEND_STUNE_BOOST, ST_TOP_APP | ST_FOREGROUND | ST_BACKGROUND, suspend_stune_boost,
+		set_stune_boost(b, SUSPEND_STUNE_BOOST, suspend_stune_boost,
 			&b->suspend_stune_slot);
 	}
 
@@ -240,9 +218,6 @@ static void cpu_input_boost_input_event(struct input_handle *handle,
 					int value)
 {
 	struct boost_drv *b = handle->handler->private;
-	u32 state;
-
-	state = get_boost_state(b);
 
 	atomic64_set(&b->prev_input_jiffies, jiffies);
 	queue_work(b->wq, &b->input_boost);
