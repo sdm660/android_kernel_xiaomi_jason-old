63905f26e609001325660ad669e84559e7ec2773
Dusan Uveric <dusan.uveric9@gmail.com>
2019-03-22 13:29:17 +0000

    cpu_input_boost: adjust implementation of df41c1d99787962c2eb2e5a09cb7c6cf8d0d1311
    Signed-off-by: Dusan Uveric <dusan.uveric9@gmail.com>

--- kernel/cgroup.c
+++ kernel/cgroup.c
@@ -2780,7 +2780,7 @@ static ssize_t __cgroup_procs_write(struct kernfs_open_file *of, char *buf,
 	if (!ret && !threadgroup &&
 		!memcmp(of->kn->parent->name, "top-app", sizeof("top-app")) &&
 		is_zygote_pid(tsk->parent->pid))
-	cpu_input_boost_kick();
+	cpu_input_boost_kick_max(1250);
 
 	put_task_struct(tsk);
 	goto out_unlock_threadgroup;
