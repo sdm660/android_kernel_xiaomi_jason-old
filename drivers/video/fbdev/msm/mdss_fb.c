1059d5c7454ef311eaa57e07760e0801447b162b
Danny Lin <danny@kdrag0n.dev>
2019-04-05 14:26:04 +0000

    msm/mdss: Add a configurable timeout for frame commit boosts
    Boosting whenever a new frame is committed can improve fluidity
    significantly, but has the unfortunate side effect of overly boosting
    during workloads that constantly update the screen but are not very
    CPU-intensive, with video playback being a key example. Over time, such
    excessive boosting can add up to a noticeable hit on battery life.
    
    To mitigate this flaw, add a configurable timeout that restricts frame
    boosting to only apply within the specified amount of time since the
    last input event was received, as input events are indicative of
    interaction.
    
    The default timeout of 3250 ms has been empirically determined by
    performing multiple flings at various speeds while timing them. The
    maximum fling time was observed to be approximately 2850 ms, so our
    frame boost timeout of 3250 ms offers some headroom for more intensive
    applications such as games.
    
    Boost timeout values:
      - negative: always boost
      - zero: never boost
      - positive: boost within timeout of X ms
    
    Signed-off-by: Danny Lin <danny@kdrag0n.dev>
    Signed-off-by: Dusan Uveric <dusan.uveric9@gmail.com>

--- drivers/video/fbdev/msm/mdss_fb.c
+++ drivers/video/fbdev/msm/mdss_fb.c
@@ -89,6 +89,9 @@
  */
 #define MDP_TIME_PERIOD_CALC_FPS_US	1000000
 
+static int frame_boost_timeout __read_mostly = CONFIG_FB_MSM_FRAME_BOOST_TIMEOUT;
+module_param(frame_boost_timeout, int, 0644);
+
 static struct fb_info *fbi_list[MAX_FBI_LIST];
 static int fbi_list_index;
 static struct msm_fb_data_type *mfd_data = NULL;
@@ -5129,6 +5132,17 @@ static bool check_not_supported_ioctl(u32 cmd)
 		(cmd == MSMFB_NOTIFY_UPDATE));
 }
 
+static void mdss_fb_kick_frame_boost(int timeout_ms)
+{
+	if (!timeout_ms)
+		return;
+
+	if (timeout_ms < 0 || cpu_input_boost_within_input(timeout_ms)) {
+		cpu_input_boost_kick();
+		devfreq_boost_kick(DEVFREQ_MSM_CPUBW);
+	}
+}
+
 /*
  * mdss_fb_do_ioctl() - MDSS Framebuffer ioctl function
  * @info:	pointer to framebuffer info
@@ -5219,8 +5233,7 @@ int mdss_fb_do_ioctl(struct fb_info *info, unsigned int cmd,
 		ret = mdss_fb_mode_switch(mfd, dsi_mode);
 		break;
 	case MSMFB_ATOMIC_COMMIT:
-		cpu_input_boost_kick();
-		devfreq_boost_kick(DEVFREQ_MSM_CPUBW);
+		mdss_fb_kick_frame_boost(frame_boost_timeout);
 		ret = mdss_fb_atomic_commit_ioctl(info, argp, file);
 		break;
 
