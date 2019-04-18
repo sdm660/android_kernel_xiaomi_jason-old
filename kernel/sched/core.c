d1cf68c05d42d7ddbce3868dddd830000bb923fc
Dusan Uveric <dusan.uveric9@gmail.com>
2019-04-18 22:39:00 +0000

    Revert "kthread: Add API to create kthreads bound to low-power CPUs"
    This reverts commit 913b6361cec17d0b2ad470be0f17aca4ffee69d4.

--- kernel/sched/core.c
+++ kernel/sched/core.c
@@ -1348,9 +1348,6 @@ static const struct cpumask *get_adjusted_cpumask(const struct task_struct *p,
 	/* Force all performance-critical kthreads onto the big cluster */
 	if (p->flags & PF_PERF_CRITICAL)
 		return cpu_perf_mask;
-	/* Force all low-power kthreads onto the little cluster */
-	if (p->flags & PF_LOW_POWER)
-		return cpu_lp_mask;
 
 	return req_mask;
 }
