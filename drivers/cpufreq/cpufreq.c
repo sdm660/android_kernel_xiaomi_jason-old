a5b8a1c63554ea94167b4d20b983d0d58c744b72
Dusan Uveric <dusan.uveric9@gmail.com>
2019-05-05 18:32:28 +0000

    Revert "cpufreq: Enforce minimum cpu freq policy"
    This reverts commit f04a854afc74af26852c3810c6470ac47de5165f.

--- drivers/cpufreq/cpufreq.c
+++ drivers/cpufreq/cpufreq.c
@@ -2398,9 +2398,6 @@ static int cpufreq_set_policy(struct cpufreq_policy *policy,
 	if (ret)
 		return ret;
 
-	if (cpumask_test_cpu(new_policy->cpu, cpu_lp_mask))
-		cpufreq_verify_within_limits(new_policy, 902400, new_policy->max);
-
 	/* notification of the new policy */
 	blocking_notifier_call_chain(&cpufreq_policy_notifier_list,
 			CPUFREQ_NOTIFY, new_policy);
